6ba5c5ce7a6a7f94e3a86876e0cc1e4eb6e86c62
Asharib706 <masharib706@gmail.com>
2024-04-03 13:15:57 +0500

    Make Home page interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,34 +1,3 @@
-const products = [{
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-
-
-},
-{
-  image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-  name: "Adults Plain Cotton T-Shirt - 2 Pack",
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-},
-{
-  image: "images/products/intermediate-composite-basketball.jpg",
-  name: "Intermediate Size Basketball",
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-}
-]
-
 let productsHTML='';
 products.forEach((product) =>{
   productsHTML+=`<div class="product-container">
@@ -74,11 +43,38 @@ products.forEach((product) =>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" 
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`
 // document.querySelector('products-grid').innerHTML+=html
 
 })
-document.querySelector('.js-products-grid').innerHTML+=productsHTML
+document.querySelector('.js-products-grid').innerHTML+=productsHTML;
+document.querySelectorAll('.js-add-to-cart').forEach( (button) =>
+{
+  button.addEventListener('click', () =>
+  {
+    let index=-1;
+
+    const productId=button.dataset.productId;
+    cart.forEach((item,i) =>{
+      if (productId===item.productId){
+        index=i;
+      }
+    })
+    if (index!==-1){
+      cart[index].quantity+=1;
+    }
+    else{
+    cart.push({
+      productId:productId,
+      quantity:1
+    });
+  }
+    console.log(cart)
+  });
+  
+});
+
