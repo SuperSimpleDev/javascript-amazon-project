fe2ce8ff6d930cab86b2978c17ea9b12f91b4b1f
Asharib706 <masharib706@gmail.com>
2024-04-06 16:35:08 +0500

    delete container from the cart when press delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
   if (matchingProduct) {
     // Construct HTML for the cart item
     const cartItemHtml = `
-      <div class="cart-item-container">
+      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: Tuesday, June 21
         </div>
@@ -99,5 +99,11 @@ document.querySelectorAll('.js-delete-link')
     const productId=link.dataset.productId;
     removeFromCart(productId);
     console.log(cart);
+
+    const container=document.querySelector(
+      `.js-cart-item-container-${productId}`
+      )
+    container.remove();
+    
   });
 });
