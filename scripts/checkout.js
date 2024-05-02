330faf822630c6d86190e4ecf788236c6657fb2c
Asharib706 <masharib706@gmail.com>
2024-05-02 21:20:49 +0500

    create a backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,6 @@
 import { renderOrderSummary } from "../checkout/orderSummary.js";
 import { renderPaymentSummary } from "../checkout/paymentSummary.js";
+import '../backend-practice.js';
 renderOrderSummary();
 renderPaymentSummary();
+
