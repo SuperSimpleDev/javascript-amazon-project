2ca751b0180fa8dd55f13284310392f65818eb63
Asharib706 <masharib706@gmail.com>
2024-05-03 15:33:49 +0500

    learn how to use fetch to load Products

--- data/products.js
+++ data/products.js
@@ -1,41 +1,41 @@
-import {formatCurrency} from "../scripts/utils/money.js"
+import { formatCurrency } from "../scripts/utils/money.js"
 
-class Product{
+class Product {
   id;
   image;
   name;
   rating;
   priceCents;
 
-  constructor(productDetails){
-  this.id=productDetails.id;
-  this.image =productDetails.image;
-  this.name=productDetails.name;
-  this.rating=productDetails.rating;
-  this.priceCents=productDetails.priceCents;
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
   }
 
-  getStarsurl(){
+  getStarsurl() {
     return `images/ratings/rating-${this.rating.stars * 10}.png`;
   }
-  getPrice(){
+  getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
 
-  extraInfoHtml(){
+  extraInfoHtml() {
     return ``
   }
 
 };
 
-class Clothing extends Product{
+class Clothing extends Product {
   sizeChartLink;
 
-  constructor  (productDetails){
+  constructor(productDetails) {
     super(productDetails);
-    this.sizeChartLink=productDetails.sizeChartLink;
+    this.sizeChartLink = productDetails.sizeChartLink;
   }
-  extraInfoHtml(){
+  extraInfoHtml() {
     return `
     <a href="${this.sizeChartLink}" target="_blank">Size Chart
     </a>
@@ -48,25 +48,48 @@ export let products = [];
 
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
-  
-  xhr.addEventListener('load', () =>{
-     
-      products = JSON.parse(xhr.response).map(productDetails => {
+
+  xhr.addEventListener('load', () => {
+
+    products = JSON.parse(xhr.response).map(productDetails => {
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      } else {
+        return new Product(productDetails);
+      }
+    });
+
+    // console.log('load Products'); // This now runs after the data is loaded
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products')
+    .then((response) => {
+      return response.json();
+
+    }).then((productsData) => {
+      products = productsData.map(productDetails => {
         if (productDetails.type === "clothing") {
           return new Clothing(productDetails);
         } else {
           return new Product(productDetails);
         }
       });
-      
-      console.log('load Products'); // This now runs after the data is loaded
-     fun();
-  });
-  
-  xhr.open('GET', 'https://supersimplebackend.dev/products');
-  xhr.send();
-}
 
+      console.log('load Products'); // This now runs after the data is loaded
+
+    });
+    return promise;
+
+}
+// loadProductsFetch().then(()=>{
+//   console.log('next step')
+// });
 
 // export const products = [
 //   {
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,15 +1,11 @@
 import { renderOrderSummary } from "../checkout/orderSummary.js";
 import { renderPaymentSummary } from "../checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts,loadProductsFetch } from "../data/products.js";
 // import '../backend-practice.js';
 import { loadCart } from "../data/cart.js";
 
 Promise.all([
-  new Promise(( resolve)=>{
-    loadProducts(()=>{  
-        resolve();
-      });
-    }),
+  loadProductsFetch(),
     new Promise((resolve)=>{
       loadCart(()=>{
         resolve();    
