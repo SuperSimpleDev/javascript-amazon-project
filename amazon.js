8233f3241e715f23296638f8b4e12c35ae8208b9
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2024-06-16 16:32:57 +0530

    Make the cart quantity interactive

--- amazon.js
+++ amazon.js
@@ -75,6 +75,10 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
           quantity: 1,
         })
       }
-      console.log(cart);
+      let cartQuantity = 0;
+      cart.forEach((item)=>{
+        cartQuantity += item.quantity;
+      });
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
   })
