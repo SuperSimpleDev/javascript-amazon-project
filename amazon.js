175e425884c270cf6835cdc0d7f9dc87e564ad02
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2024-06-16 16:49:40 +0530

    removing extra space

--- amazon.js
+++ amazon.js
@@ -6,11 +6,9 @@ products.forEach((product) => {
             <img class="product-image"
               src="${product.image}">
           </div>
-
           <div class="product-name limit-text-to-2-lines">
            ${product.name}
           </div>
-
           <div class="product-rating-container">
             <img class="product-rating-stars"
               src="images/ratings/rating-${product.rating.stars * 10}.png">
@@ -20,11 +18,9 @@ products.forEach((product) => {
               }
             </div>
           </div>
-
           <div class="product-price">
             $${(product.priceCents / 100).toFixed(2)}
           </div>
-
           <div class="product-quantity-container">
             <select>
               <option selected value="1">1</option>
@@ -39,14 +35,11 @@ products.forEach((product) => {
               <option value="10">10</option>
             </select>
           </div>
-
           <div class="product-spacer"></div>
-
           <div class="added-to-cart">
             <img src="images/icons/checkmark.png">
             Added
           </div>
-
           <button class="add-to-cart-button button-primary js-add-to-cart" 
           data-product-id="${product.id}">
             Add to Cart
@@ -55,13 +48,10 @@ products.forEach((product) => {
   `;
 });
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
     button.addEventListener('click',()=>{
       const productId = button.dataset.productId;
-
       let matchingItem;
-
       cart.forEach((item)=>{
         if(productId === item.productId){
           matchingItem = item;
