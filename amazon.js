9f0cdaa21f3e61171e3073a9582463bbf8321e6b
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2024-06-17 20:28:24 +0530

    final touchup

--- amazon.js
+++ amazon.js
@@ -74,11 +74,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
       });
       document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
       const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
-      addedMessage.classList.add('added-to-cart-visible')
+      addedMessage.classList.add('added-to-cart-visible');
       if(addedMessageTimeoutId){clearTimeout(addedMessageTimeoutId);
       }
       const timeoutId = setTimeout(()=>{
         addedMessage.classList.remove('added-to-cart-visible')
-      },1500)
+      },1500);
+      addedMessageTimeoutId = timeoutId;
     });
-  })
+  });
